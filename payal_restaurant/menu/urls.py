c27e1e54f136872b10ff6296fca8cc2862a42dc1
Rajat <rajatdave001@gmail.com>
2022-07-21 22:54:52 +0530

    last commit of the day

--- payal_restaurant/menu/urls.py
+++ payal_restaurant/menu/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
    
     path('', views.home),
+    path('north_indian/', views.north_indian_menu, name = 'north_indian'),
+
 
 ]
--- payal_restaurant/menu/views.py
+++ payal_restaurant/menu/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'Base.html')
+
+
+def north_indian_menu(request):
+    return render(request, 'north_indian.html')
