17fa8e8cc516ef7f316a91376a5b27928ef4d9fd
Rajat <rajatdave001@gmail.com>
2022-07-21 18:23:50 +0530

    Shef & Menu Created

--- payal_restaurant/menu/models.py
+++ payal_restaurant/menu/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from zmq import DISH
 
 # Create your models here.
+
+class Shef(models.Model):
+    shef_id = models.IntegerField(max_length=5)
+    shef_name = models.CharField(max_length=5)
+
+class Menu(models.Model):
+    Dish_Name = models.CharField(max_length=30)
+    Dish_Quantity = models.IntegerField()
+    
+
+    
